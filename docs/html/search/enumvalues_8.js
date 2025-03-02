4fb65649a77d72ab1691482f8db349fa42c7bde9
Byterset <23340789+Byterset@users.noreply.github.com>
2025-03-02 01:25:49 +0100

    move doxygen docs

--- docs/html/search/enumvalues_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['type_5flocation_5fstring_2118',['TYPE_LOCATION_STRING',['../scene__loader_8c.html#ae02fbe85f939a0eb41fc3cd7ed4cfbadaf03f07292d2bf90b53c61bb9130b3dcd',1,'scene_loader.c']]]
-];
